b9519a8716db2969c5112c39ce577e303f61b12c
Matthew Becker <mateo62009mp@gmail.com>
2026-01-12 19:15:19 -0300

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,35 @@
+from telethon import events
+
+# Importamos las funciones de los archivos hermanos
+from .welcome import handler_welcome, handler_hello
+from .general import handler_help, handler_cmds, handler_request, handler_urldebug
+from .status import handler_status
+from .shop import handler_list, handler_info, handler_buy
+from .admin import handler_secret_menu, handler_add, handler_del, handler_edit
+
+def register_all_handlers(client):
+    """Aquí unimos cada función con su comando (pattern)"""
+    
+    # Welcome
+    client.add_event_handler(handler_welcome, events.ChatAction)
+    client.add_event_handler(handler_hello, events.NewMessage(pattern=r'\.hello'))
+    
+    # General
+    client.add_event_handler(handler_help, events.NewMessage(pattern=r'\.help'))
+    client.add_event_handler(handler_cmds, events.NewMessage(pattern=r'\.cmds'))
+    client.add_event_handler(handler_request, events.NewMessage(pattern=r'\.request(?:\s+(.*))?'))
+    client.add_event_handler(handler_urldebug, events.NewMessage(pattern=r'\.urldebug(?:\s+(.*))?'))
+    
+    # Status
+    client.add_event_handler(handler_status, events.NewMessage(pattern=r'\.status(?:\s+(.*))?'))
+    
+    # Shop
+    client.add_event_handler(handler_list, events.NewMessage(pattern=r'\.list'))
+    client.add_event_handler(handler_info, events.NewMessage(pattern=r'\.info(?:\s+(.*))?'))
+    client.add_event_handler(handler_buy, events.NewMessage(pattern=r'\.buy(?:\s+(.*))?'))
+    
+    # Admin
+    client.add_event_handler(handler_secret_menu, events.NewMessage(outgoing=True, pattern=r'\.2284230134'))
+    client.add_event_handler(handler_add, events.NewMessage(outgoing=True, pattern=r'\.add\s+(.*)'))
+    client.add_event_handler(handler_del, events.NewMessage(outgoing=True, pattern=r'\.del\s+(.*)'))
+    client.add_event_handler(handler_edit, events.NewMessage(outgoing=True, pattern=r'\.edit\s+(.*)'))
