61e24f7637d5cd283fcb2667a3180a056ebc796c
Matthew Becker <mateo62009mp@gmail.com>
2026-03-17 15:38:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telethon.sessions import StringSession
 import config
 from database import db
 from server import start_server
-import handlers # Importamos la carpeta que creamos
+import handlers
 
 if not config.SESSION_STRING:
     print("❌ CRITICAL: MISSING SESSION_STRING")
@@ -21,18 +21,18 @@ async def main():
     await db.connect()
     
     print("🧠 3. Loading Handlers...")
-    # Esto llama al __init__.py de la carpeta handlers
     handlers.register_all_handlers(client)
     
     print("🚀 4. Login to Telegram...")
     await client.start()
     
-    try: await client.send_message("me", "🚀 **SYSTEM ONLINE (Split Version 10.0)**")
+    try: await client.send_message("me", "🚀 **SYSTEM ONLINE (Split Version 121.0)**")
     except: pass
     
     print("✅ BOT IS RUNNING!")
     await client.run_until_disconnected()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     loop.run_until_complete(main())
