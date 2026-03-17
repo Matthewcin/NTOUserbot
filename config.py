2e7003f8e3cdf8c438c5b77e08df31a4821cdbde
Matthew Becker <mateo62009mp@gmail.com>
2026-03-17 15:22:24 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,14 @@ import os
 
 API_ID = int(os.getenv('API_ID', '32541501'))
 API_HASH = os.getenv('API_HASH', '66f7a1c72eac5d25705ef1d35275ca4f')
-SESSION_STRING = os.getenv('SESSION_STRING')
-
-DB_URL = os.getenv('DB_URL')
+SESSION_STRING = os.getenv('SESSION_STRING', '1AZWarzgBu5zYZqEtCJEubS8RBfVaS97dS3Y0wpXXrOEeBCsO3byYcEPp5fcwKacSl9KP_aexNFRj4mzTDPYAHW3Il0E5WfjUuA_ECiax9id-zaxyi1DmDl29zi7Qz20ztxVCEfYLqa3v07JcI1xV9pLh07UMTBM0p5rRmZHp2-Ga0kFTbPzSqhsxOo7LiMvDdaSoV6kBTbAgQJ1A7obgXBRBmRwj8sLfWwOtLPPYj2Bkp97pjjge1XZxditfdg6uRzPZFWICmfYkoimMIwIgL-ZF8z3Gjfaaf__Yzi0PPhjDGhpmAcL5fa1zDH6kOLAY5A5o7h8MxPjrPQ2RBfP1gFyQO1ODseM=')
+DB_URL = os.getenv('DB_URL', 'postgresql://neondb_owner:npg_Nd2D8BMWYGXj@ep-flat-snow-ah0dg0ip-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
 
 # Configuración de Crypto
 FREE_CRYPTO_API_TOKEN = "4hgzw1dnorurolqkpcg1"
 
 # ☁️ OpenBullet API
-OB_URL = "http://cloudfig7-001-site1.rtempurl.com/top"
+OB_URL = "http://cloudfig8-001-site1.mtempurl.com/top"
 OB_SECRET = "ZjBmYjQ2ZTdiMmRiZTdmYzAxNjY1Nzc1MjVkNGNlY2JjYmU3NWQwYjNiNGIzMWU0MTVlYTcxMWM5MmY3MWRkNmQzMzU1MjYzOGY4OWZjMjg2NzlhMjg1ZjZhMTEyOWZmMmJiZjYyMGM1Y2VhMjMwMDE3YzdmODJiNGJjM2RmN2Q="
 
 TARGET_GROUP = 'myConfigCloud'
