063d47c3637e6bf68e72fbd1c364fffcdb963c60
Matthew Becker <mateo62009mp@gmail.com>
2026-07-12 22:55:07 -0300

    Update TARGET_GROUP in config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.getenv('API_ID', '32541501'))
 API_HASH = os.getenv('API_HASH', '66f7a1c72eac5d25705ef1d35275ca4f')
-SESSION_STRING = os.getenv('SESSION_STRING', '1AZWarzgBu5zYZqEtCJEubS8RBfVaS97dS3Y0wpXXrOEeBCsO3byYcEPp5fcwKacSl9KP_aexNFRj4mzTDPYAHW3Il0E5WfjUuA_ECiax9id-zaxyi1DmDl29zi7Qz20ztxVCEfYLqa3v07JcI1xV9pLh07UMTBM0p5rRmZHp2-Ga0kFTbPzSqhsxOo7LiMvDdaSoV6kBTbAgQJ1A7obgXBRBmRwj8sLfWwOtLPPYj2Bkp97pjjge1XZxditfdg6uRzPZFWICmfYkoimMIwIgL-ZF8z3Gjfaaf__Yzi0PPhjDGhpmAcL5fa1zDH6kOLAY5A5o7h8MxPjrPQ2RBfP1gFyQO1ODseM=')
+SESSION_STRING = os.getenv('SESSION_STRING', '1AZWarzYBuzkasjhTpFrUaw0ShyZ6HhWO-aPTZ3z24cW3bliFzOHT7V0mde1lQrLuFiwCVInd2uigPeixUjbipUXbyTkvx56BOEWSqFexbDcIOGIwFXYFN-HZ_p71s4Q5Sg05PzLGhsgpuLebF5mK1mGU6xlRxBpHrQNhr6ZdW3vtev6qghTLOLUiE9mF3lvxM22KJGukpZMDghqq8InJ1kNTY9y9biz3GW3OYrFgbpNRL5_IW4Re1rvwTNLPwAf-pvTbfAn0iffnavkaBdKq_FaZCkIpxy6PzOgsxSSXPTNHnk7Yiq6HG1NxNQ192jmvbBUgrWU8NCL8aEN3c4-GEC3UXH7Ns-0=')
 DB_URL = os.getenv('DB_URL', 'postgresql://neondb_owner:npg_Nd2D8BMWYGXj@ep-flat-snow-ah0dg0ip-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
 
 # Configuración de Crypto
@@ -12,7 +12,7 @@ FREE_CRYPTO_API_TOKEN = "4hgzw1dnorurolqkpcg1"
 OB_URL = "https://cloudfig10-001-site1.ktempurl.com/top"
 OB_SECRET = "ZjBmYjQ2ZTdiMmRiZTdmYzAxNjY1Nzc1MjVkNGNlY2JjYmU3NWQwYjNiNGIzMWU0MTVlYTcxMWM5MmY3MWRkNmQzMzU1MjYzOGY4OWZjMjg2NzlhMjg1ZjZhMTEyOWZmMmJiZjYyMGM1Y2VhMjMwMDE3YzdmODJiNGJjM2RmN2Q="
 
-TARGET_GROUP = 'myConfigCloud'
+TARGET_GROUP = 'myConfigCloud2'
 MY_USER_LINK = 'https://t.me/whois_tyler'
 STICKER_FILENAME = 'sticker.tgs'
 GUIDE_FILENAME = 'guide.jpg'
