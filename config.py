60b964518a69d49ae89c2e26c9fc4358b7ef2e09
Matthew Becker <mateo62009mp@gmail.com>
2026-07-27 03:07:56 -0300

    Update SESSION_STRING in config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.getenv('API_ID', '32541501'))
 API_HASH = os.getenv('API_HASH', '66f7a1c72eac5d25705ef1d35275ca4f')
-SESSION_STRING = os.getenv('SESSION_STRING', '1AZWarzYBu56FCGYaqGi83Eytv-5ySrsXKQvpeWvR8gyLvXC7gzt6MgwlDEHesiRsfCEZF7tfz65enhINqdDoepKuCTMZdUnTbUJOZVt1h2ylDJnVAQzqCmQiRc2Gkdj5oy6GSD2UUSnCXuxxDGzLdaf0w3WNHghSyGWnpK7QJnbmQ5aiUq5lzhho6sWsVJUAY7y3r79gqST2gXIaNwMeZvFbf9IRvT8s_smEMmvVEpPs5_JvYNqizL_jN5jqXHEUxKpsThbhKTN9spqRBGj6JoidAoAE3fO_gwif3D9NqsfjJEMofoKp8KTXTag5qq4GrqEVkLrxgIynbFa4cyH27bkjCHnVsGs=')
+SESSION_STRING = os.getenv('SESSION_STRING', '1AZWarzYBuwVlEb4YX2_J9zrYyFvXnZsfK_Pgr-g7KLIQSzD6yqZn-h2ZCvm9xKXwxOgP6Jag0tsGCpPWhEJgUXO6caozmhvqyyGilBUofpCJRM-19-Bic2cpULd8gzbTSCPUr2528BRQfIewOkz_VJmMx-iwy3L7c0Xxy71p81bJCkNZSKfxFWxnQQD_x423tZQBYVyOh1lSj9SQTJEWHIqIBmopTlS57rQ_LvfE6q1aa2uPSTcbEXr_fHSk91Fz7Se-3sp2ze_jcKpqqBj1oAxpTDhkqWgK198qhN2RdYht9p3_ng3WIXKYut0ZUToeJvqLFR5URG8xLKiR64s2XKnvEnzx0lQ=')
 DB_URL = os.getenv('DB_URL', 'postgresql://neondb_owner:npg_Nd2D8BMWYGXj@ep-flat-snow-ah0dg0ip-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
 
 # Configuración de Crypto
